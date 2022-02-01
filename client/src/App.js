40bbbf9f36d9ce6ae9ca67fbb10358f56ac287ed
Azachary8232 <azachary8232@hotmail.com>
2022-01-31 18:44:54 -0700

    Main Page Wire Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
+import Main from './views/Main';
+import Test from './views/Test'
 
-      // VIEWS / CONTROLLERS IMPORTS
-import Test from './views/Test';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route to="/test">
-            <Test/>
+          <Route to='/main'>
+            <Main/>
           </Route>
+          {/* <Route to='/test'>
+            <Test/>
+          </Route> */}
         </Switch>
       </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/views/Main.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import '../css/style.css';
+
+
+const Main = (props) => {
+
+
+return(
+    <div>
+        <h1>Main Page</h1>
+        <div className='container2 lego'>
+            {/* NavBar Component */}
+        </div>
+        <div>
+            {/* Container */}
+            <div>
+                {/* left column */}
+                <div>
+                    {/* Upper Box */}
+                    <div>
+                        {/* Drop Down Component */}
+                    </div>
+                    <div>
+                        {/* Carousel Component */}
+                    </div>
+                </div>
+                <div>
+                    {/* Lower Box */}
+                    <div>
+                        {/* Drop Down Component */}
+                    </div>
+                    <div>
+                        {/* Carousel Component */}
+                    </div>
+                </div>
+            </div>
+            <div>
+                {/* display */}
+            </div>
+            <div>
+                {/* right column */}
+                <div>
+                    {/* Upper Box */}
+                    <div>
+                        {/* Drop Down Component */}
+                    </div>
+                    <div>
+                        {/* Carousel Component */}
+                    </div>
+                </div>
+                <div>
+                    {/* Lower Box */}
+                    <div>
+                        {/* Drop Down Component */}
+                    </div>
+                    <div>
+                        {/* Carousel Component*/}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+)
+
+}
+export default Main;
